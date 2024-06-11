4f40c6a0e695f645f2552a9ea375254ffacd2a48
ksithu555 <ksithu555@gmail.com>
2024-06-11 17:43:18 +0900

    scroll color and guest js

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -910,49 +910,6 @@
     <!-- theme setting js -->
     <script src="{{ asset('frontend/assets/js/theme-setting.js') }}"></script>
 
-    <!-- latest jquery-->
-    <script src="{{ asset('frontend/assets/js/jquery-3.6.0.min.js') }}"></script>
-
-    <!-- jquery ui-->
-    <script src="{{ asset('frontend/assets/js/jquery-ui.min.js') }}"></script>
-
-    <!-- Bootstrap js-->
-    <script src="{{ asset('frontend/assets/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
-
-    <script src="{{ asset('frontend/assets/js/bootstrap/bootstrap-notify.min.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/bootstrap/popper.min.js') }}"></script>
-
-    <!-- feather icon js-->
-    <script src="{{ asset('frontend/assets/js/feather/feather.min.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/feather/feather-icon.js') }}"></script>
-
-    <!-- Lazyload Js -->
-    <script src="{{ asset('frontend/assets/js/lazysizes.min.js') }}"></script>
-
-    <!-- Slick js-->
-    <script src="{{ asset('frontend/assets/js/slick/slick.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/slick/slick-animation.min.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/slick/custom_slick.js') }}"></script>
-
-    <!-- Auto Height Js -->
-    <script src="{{ asset('frontend/assets/js/auto-height.js') }}"></script>
-
-    <!-- Timer Js -->
-    <script src="{{ asset('frontend/assets/js/timer1.js') }}"></script>
-
-    <!-- Quantity js -->
-    <script src="{{ asset('frontend/assets/js/quantity-2.js') }}"></script>
-
-    <!-- WOW js -->
-    <script src="{{ asset('frontend/assets/js/wow.min.js') }}"></script>
-    <script src="{{ asset('frontend/assets/js/custom-wow.js') }}"></script>
-
-    <!-- script js -->
-    <script src="{{ asset('frontend/assets/js/script.js') }}"></script>
-
-    <!-- theme setting js -->
-    <script src="{{ asset('frontend/assets/js/theme-setting.js') }}"></script>
-
     <script>
         $(document).ready(function() {
             $.ajaxSetup({
