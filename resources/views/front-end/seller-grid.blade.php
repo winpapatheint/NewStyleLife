74410954d5222f537d846ef59ee730e1fa88f9ee
ksithu555 <ksithu555@gmail.com>
2024-06-27 14:01:25 +0900

    shop phone format

--- resources/views/front-end/seller-grid.blade.php
+++ resources/views/front-end/seller-grid.blade.php
@@ -27,7 +27,8 @@
     <!-- Grid Section Start -->
     <section class="section-b-space seller-grid-section">
         @php
-            function formatZipCode($zipCode) {
+            function formatZipCode($zipCode)
+            {
                 if (preg_match('/^\d{3}-\d{4}$/', $zipCode)) {
                     return $zipCode;
                 }
@@ -36,76 +37,108 @@
                 }
                 return $zipCode; // return as-is if not a standard 7 digit zip code
             }
+            function formatPhoneNumber($phone)
+            {
+                // Remove any non-digit characters
+                $phone = preg_replace('/\D+/', '', $phone);
+
+                // Assume international format if the phone number starts with '00' or '+'
+                if (substr($phone, 0, 2) == '00') {
+                    $phone = substr($phone, 2);
+                } elseif (substr($phone, 0, 1) == '+') {
+                    $phone = substr($phone, 1);
+                } else {
+                    // Default country code (change '81' to your default country code)
+                    $phone = '' . $phone;
+                }
+
+                // Format the phone number (example: +81 80 3829 8333)
+                $formatted =
+                    '+' .
+                    substr($phone, 0, 2) .
+                    ' ' .
+                    substr($phone, 2, 3) .
+                    ' ' .
+                    substr($phone, 5, 4) .
+                    ' ' .
+                    substr($phone, 9);
+
+                return $formatted;
+            }
         @endphp
         <div class="container-fluid-lg">
             <div class="row g-4">
-                @if($lists->count() < 1)
-                        <h1 class="text-center">No Shop Available</h1>
+                @if ($lists->count() < 1)
+                    <h1 class="text-center">No Shop Available</h1>
                 @endif
-                @foreach($lists as $shop => $seller)
-                <div class="col-xxl-4 col-md-6">
-                    <a href="{{ url("/shopleftsidebar/".$seller->user_id ) }}" class="seller-grid-box">
-                        <div class="grid-contain">
-                            <div class="seller-contact-details">
-                                <div class="seller-contact">
-                                    <div class="seller-icon">
-                                        <i class="fa-solid fa-map-pin"></i>
+                @foreach ($lists as $shop => $seller)
+                    <div class="col-xxl-4 col-md-6">
+                        <a href="{{ url('/shopleftsidebar/' . $seller->user_id) }}" class="seller-grid-box">
+                            <div class="grid-contain">
+                                <div class="seller-contact-details">
+                                    <div class="seller-contact">
+                                        <div class="seller-icon">
+                                            <i class="fa-solid fa-map-pin"></i>
+                                        </div>
+
+                                        <div class="contact-detail">
+                                            <h5>Address: <span> 〒{{ $seller->zip_code }}</span>
+                                                <span>{{ $seller->country->name }} {{ $seller->prefecture }}
+                                                    {{ $seller->city }} {{ $seller->chome }} {{ $seller->building }}
+                                                    {{ $seller->room }}</span>
+                                            </h5>
+                                        </div>
                                     </div>
 
-                                    <div class="contact-detail">
-                                        <h5>Address: <span> 〒{{ $seller->zip_code }}</span>
-                                            <span>{{ $seller->country->name}} {{ $seller->prefecture }} {{ $seller->city }} {{ $seller->chome }} {{ $seller->building }} {{ $seller->room }}</span></h5>
+                                    <div class="seller-contact">
+                                        <div class="seller-icon">
+                                            <i class="fa-solid fa-phone"></i>
+                                        </div>
+
+                                        <div class="contact-detail">
+
+                                            <h5>Contact Us: <span>{{ formatPhoneNumber($seller->phone) }}</span></h5>
+                                        </div>
                                     </div>
                                 </div>
-
-                                <div class="seller-contact">
-                                    <div class="seller-icon">
-                                        <i class="fa-solid fa-phone"></i>
-                                    </div>
-
-                                    <div class="contact-detail">
-
-                                        <h5>Contact Us: <span>{{ $seller -> phone }}</span></h5>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="contain-name">
-                                <div>
-                                    <h6>Since {{ \Carbon\Carbon::parse($seller->shop_establish)->format('Y') }}
-                                    </h6>
-                                    <h3>{{ $seller->shop_name }}</h3>
-                                    <div class="product-rating">
-                                        <ul class="rating">
-                                            @for ($i = 1; $i <= 5; $i++)
-                                                @if ($i <= $ratingWithProductCount[$shop][0])
-                                                    <li><i data-feather="star" class="fill"></i></li>
-                                                @else
-                                                    <li><i data-feather="star"></i></li>
-                                                @endif
-                                            @endfor
-                                        </ul>
-                                        <span>({{ $ratingWithProductCount[$shop][1] }} <?php echo ($ratingWithProductCount[$shop][1] > 1) ? 'Reviews' : 'Review'; ?>)</span>
-                                    </div>
-                                    @php $shopProductCount = 0; @endphp
-                                    @foreach ($seller->user->products as $product)
-                                        @if ($product->status == 1)
-                                            @php $shopProductCount++; @endphp
+                                <div class="contain-name">
+                                    <div>
+                                        <h6>Since {{ \Carbon\Carbon::parse($seller->shop_establish)->format('Y') }}
+                                        </h6>
+                                        <h3>{{ $seller->shop_name }}</h3>
+                                        <div class="product-rating">
+                                            <ul class="rating">
+                                                @for ($i = 1; $i <= 5; $i++)
+                                                    @if ($i <= $ratingWithProductCount[$shop][0])
+                                                        <li><i data-feather="star" class="fill"></i></li>
+                                                    @else
+                                                        <li><i data-feather="star"></i></li>
+                                                    @endif
+                                                @endfor
+                                            </ul>
+                                            <span>({{ $ratingWithProductCount[$shop][1] }} <?php echo $ratingWithProductCount[$shop][1] > 1 ? 'Reviews' : 'Review'; ?>)</span>
+                                        </div>
+                                        @php $shopProductCount = 0; @endphp
+                                        @foreach ($seller->user->products as $product)
+                                            @if ($product->status == 1)
+                                                @php $shopProductCount++; @endphp
+                                            @endif
+                                        @endforeach
+                                        @if ($shopProductCount > 1)
+                                            <span class="product-label">{{ $shopProductCount }} Products</span>
+                                        @else
+                                            <span class="product-label">{{ $shopProductCount }} Product</span>
                                         @endif
-                                    @endforeach
-                                    @if ($shopProductCount > 1)
-                                    <span class="product-label">{{ $shopProductCount }} Products</span>
-                                    @else
-                                    <span class="product-label">{{ $shopProductCount }} Product</span>
-                                    @endif
-                                </div>
+                                    </div>
 
-                                <div class="grid-image">
-                                    <img src="{{ asset('images/'.($seller->shop_logo)   ) }}" alt="" class="img-fluid">
+                                    <div class="grid-image">
+                                        <img src="{{ asset('images/' . $seller->shop_logo) }}" alt=""
+                                            class="img-fluid">
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </a>
-                </div>
+                        </a>
+                    </div>
                 @endforeach
 
             </div>
