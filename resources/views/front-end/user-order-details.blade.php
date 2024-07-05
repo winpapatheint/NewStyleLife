5ead4aefe21c14406c769f6e51bbce21c92ec145
ksithu555 <ksithu555@gmail.com>
2024-07-05 16:03:11 +0900

    order status check fixed

--- resources/views/front-end/user-order-details.blade.php
+++ resources/views/front-end/user-order-details.blade.php
@@ -90,11 +90,6 @@
                                         type="button" role="tab" style="font-size: 14px; text-align: center;" href="{{route ('user_addresses')}}"><i
                                             data-feather="map-pin"></i>Addresses</a>
                                 </li>
-                                {{-- <li class="nav-item" role="presentation">
-                                    <a class="nav-link" id="pills-card-tab"
-                                        type="button" role="tab" style="font-size: 14px; text-align: center;" href="{{route ('user_cards')}}"><i
-                                            data-feather="credit-card"></i>Payment Methods</a>
-                                </li> --}}
                                 <li class="nav-item" role="presentation">
                                     <a class="nav-link" id="pills-profile-tab"
                                         type="button" role="tab" style="font-size: 14px; text-align: center;" href="{{route ('user_profile')}}"><i data-feather="user"></i>
@@ -188,7 +183,7 @@
                                                                     </td>
                                                                     <td>
                                                                         <a href="{{ route('show-product-left-thumbnail', ['id' => $order->product_id]) }}">
-                                                                            <h5 style="width: 100px;">{{ $order->product_name }}</h5>
+                                                                            <h5 style="width: 100px;">{{ mb_substr($order->product_name, 0, 20) . '...' }}</h5>
                                                                         </a>
                                                                     </td>
                                                                     @php
@@ -211,12 +206,12 @@
                                                                     </td>
                                                                     <td>
                                                                         @if ($order->payment_approved == 1)
-                                                                            @if ($order->status != "Cancel" || $order->status != "Cash Cancel")
+                                                                            @if ($order->status != "Cancel" && $order->status != "Cash Cancel")
                                                                             <a type="button" class="btn btn-sm" style="background-color: var(--theme-color); border:0.5px solid var(--theme-color); margin-left:0.5em; color:white;"
                                                                                 href="{{route ('order_detail_tracking',['id' => $order->order_detail_id]) }}">Tracking</a>
                                                                             @else
                                                                             <button type="button" class="btn btn-sm" style="background-color: #ff6b6b; border:0.5px solid var(--theme-color); margin-left:0.5em; color:white;"
-                                                                                onclick="cancelReason({{ $order->order_detail_id }})">Canceled</button>
+                                                                                onclick="cancelReason({{ $order->order_detail_id }})">Cancelled</button>
                                                                             @endif
                                                                         @endif
                                                                     </td>
